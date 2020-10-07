1f424d1c25ca9d8d3bbd989ad2875d8a92e70e53
vagrant <vagrant@vagrant.vm>
2020-10-07 00:56:58 +0000

    made tuples for the first 2 words and lists for the third word

--- markov.py
+++ markov.py
@@ -45,10 +45,36 @@ def make_chains(text_string):
     chains = {}
 
     # your code goes here
+    # loop over words, first 2 words is the tuple, 3rd word is a value
+    # move forward in list by 1
+    # create list if first and second word aren't there, otherwise add it to list
+
     contents = open_and_read_file("green-eggs.txt")
     words = contents.split()
-    for i in range(len(words) - 1):
-        print(words[i], words[i + 1])
+
+    for i in range(0, len(words) - 3):
+        # word_list = [words[i], words[i + 1], words[i + 2]] 
+        tuple_word = (words[i], words[i + 1])
+        list_word = [words[i + 3]]
+        print(tuple_word, list_word)
+
+    # for word in word_list:
+    #     (word[i], word[i + 1])
+    #     print(word_list)
+
+    # tuple_words = [(words[i], words[i + 1]) for i in range(0, len(words) - 1)]
+    # key = tuple_words
+    # value = [words[i + 2] for i in range(len(words) - 2)]
+    # chains[key] = value
+
+    # print(f"tuples {tuple_words}")
+
+    # list_values = list.extend(words[i + 2])
+    # print(list_values)
+    # for words in contents:
+    #     list = []
+    #     list_values = list.extend(words[i + 2])
+    #     print(list_values)
 
     return chains
 
